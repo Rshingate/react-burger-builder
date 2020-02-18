6907644b848c7578e121b415a449a10aef2ddf1e
RahulS2407 <rahul.shingate@neosofttech.com>
2020-02-18 17:58:32 +0530

    config files

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Layout from './components/Layout/Layout';
 
 function App() {
+
   return (
     <div className="App">
-
+        <Layout></Layout>
     </div>
   );
 }
